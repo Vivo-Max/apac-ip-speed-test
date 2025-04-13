8472a914961170b5fc61fa44acba2b3769cb9ce3
M_107 <1173287796@qq.com>
2025-04-13 18:00:41 +0800

    Update iptest.sh

--- iptest.sh
+++ iptest.sh
@@ -1,3 +1,3 @@
    #!/bin/bash
 
-   ./iptest -file=ip.txt -tls=true -speedtest=10 -speedlimit=8 timeout=5 -url="speed.cloudflare.com/__down?bytes=1000000" -max=100 -outfile="ip.csv"
+   ./iptest -file=ip.txt -tls=true -speedtest=2 -speedlimit=8 timeout=5 -url="speed.cloudflare.com/__down?bytes=1000000" -max=100 -outfile="ip.csv"
